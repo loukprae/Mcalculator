c17eb76e3b0d2cc76a2519476e2dded89a91667d
loukprae <onnutcha.k@ku.th>
2025-10-20 03:43:21 +0700

    Fix NaN error

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const display = document.getElementById('display')
+//Calculator
+
+const  display = document.getElementById('display');
 
 function toDisplay(input){
     display.value += input;
@@ -8,28 +10,39 @@ function clearBtn(){
     display.value = '';
 }
 
-function calculate(){
 
+
+function calculate(){
+    
     let text = display.value;
+
+    
     let cal = text.split(/([+\-*/])/).filter(x => x.trim != '');
 
-   for(i=0; i < cal.length; i++){
-        if(cal[i] === "*" || cal[i] === "/"){
-        let left = parseFloat(cal[i-1]);
-        let right = parseFloat(cal[i+0]);
-        let result = cal[i] === '*' ? left * right : left / right;
-        cal.splice(i-1,3,result);
-        i--;}
-   }
+    
+    for(i = 0; i < cal.length; i++){
+        if(cal[i] === '*' || cal[i] === '/'){
+            let left = parseFloat(cal[i-1]);
+            let right = parseFloat(cal[i+1]);
+            let result = cal[i] === '*' ? left * right : left / right;
+           
+            cal.splice(i-1,3,result);
+            i--;
+        }
+    }
 
-   let result = parseFloat(cal[0]);
+    
+    let result = parseFloat(cal[0]);
 
-   for(let i = 1; i < cal.length; i += 2){
-    let op = cal[i];
-    let num = parseFloat(cal[i+1]);
-    if(op === '+') result += num;
-    else if (op === '-') result -= num;
-   }
+    for(let i = 1; i < cal.length; i += 2){
+        let op = cal[i];
+        let num = parseFloat(cal[i+1]);
+        if (op === '+') result += num;
+        else if (op === '-') result -= num;
+    }
 
-   display.value = result;
+    display.value = result;
+
+
 }
+
